d9828a90c9997cd26653f8bf1da2dc5d7bfc1c29
Bersulang <bersulang0627@gmail.com>
2025-09-22 17:56:48 +0800

    Update hero section titles and links to new site

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,10 +20,10 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'lazypigzyh.top',
+  HEO_HERO_TITLE_3: 'Bersulang.top',
   HEO_HERO_TITLE_4: '新版上线！',
   HEO_HERO_TITLE_5: '更多精彩敬请期待！',
-  HEO_HERO_TITLE_LINK: 'https://www.lazypigzyh.top/',
+  HEO_HERO_TITLE_LINK: 'https://www.bersulang.top/',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
@@ -53,7 +53,7 @@ const CONFIG = {
   HEO_INFO_CARD_ICON1: 'fas fa-user',
   HEO_INFO_CARD_URL2: 'https://github.com/Bersulang',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.lazypigzyh.top/',
+  HEO_INFO_CARD_URL3: 'https://www.bersulang.top/',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -120,7 +120,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://www.lazypigzyh.top/',
+  HEO_SOCIAL_CARD_URL: 'https://www.bersulang.top/',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
